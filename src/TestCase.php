8d3c295c1ab354bab9445e3c5c4b0a4e346ded29
Christian Ramelow <info@codekandis.net>
2019-12-27 18:18:16 +0100

    Adds the class `TestCase`.

---
+++ src/TestCase.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\PhpUnit;
+
+use PHPUnit\Framework\TestCase as TestCaseOrigin;
+
+/**
+ * Represents the base class for all test cases.
+ * @package codekandis/phpunit
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class TestCase extends TestCaseOrigin
+{
+}
