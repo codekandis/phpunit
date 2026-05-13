36ccc0e7c4e60b1c4794bb772cde3d2d23931725
Christian Ramelow <info@codekandis.net>
2026-05-13 11:40:26 +0200

    Adds `TestCase` failure method for missing expected throwables
    * adds
      * `EXPECTED_THROWABLE_HAS_NOT_BEEN_THROWN_MESSAGE` for missing expected throwable failures
      * `failExpectedThrowableHasNotBeenThrown()` to fail exception tests through `static::fail()`
    * updates
      * `TestCase` imports to include `AssertionFailedError` for the failure method `@throws` contract

--- src/TestCase.php
+++ src/TestCase.php
@@ -7,6 +7,7 @@ use CodeKandis\PhpUnit\Constraints\IsKeyedSubsetOfArrayConstraint;
 use CodeKandis\PhpUnit\Constraints\IsSubClassOfConstraint;
 use CodeKandis\PhpUnit\Constraints\IsUnkeyedSubsetOfArrayConstraint;
 use Override;
+use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\TestCase as TestCaseOrigin;
 
 /**
@@ -16,6 +17,22 @@ use PHPUnit\Framework\TestCase as TestCaseOrigin;
  */
 abstract class TestCase extends TestCaseOrigin implements TestCaseInterface
 {
+	/**
+	 * Represents the expected throwable has not been thrown message.
+	 * @var string
+	 */
+	protected const string EXPECTED_THROWABLE_HAS_NOT_BEEN_THROWN_MESSAGE = 'The expected throwable has not been thrown.';
+
+	/**
+	 * Fails because an expected throwable has not been thrown.
+	 * @return never
+	 * @throws AssertionFailedError The test failed.
+	 */
+	protected static function failExpectedThrowableHasNotBeenThrown(): never
+	{
+		static::fail( static::EXPECTED_THROWABLE_HAS_NOT_BEEN_THROWN_MESSAGE );
+	}
+
 	/**
 	 * {@inheritDoc}
 	 */
